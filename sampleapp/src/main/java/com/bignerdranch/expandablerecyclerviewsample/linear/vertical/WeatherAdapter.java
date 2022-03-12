7912931fddc19a151b94c76aff80ed48431dccd1
Tony Tang <tonytang@uber.com>
2022-03-11 20:45:09 -0800

    Clean up adapter.

--- sampleapp/src/main/java/com/bignerdranch/expandablerecyclerviewsample/linear/vertical/WeatherAdapter.java
+++ sampleapp/src/main/java/com/bignerdranch/expandablerecyclerviewsample/linear/vertical/WeatherAdapter.java
@@ -1,11 +1,11 @@
 package com.bignerdranch.expandablerecyclerviewsample.linear.vertical;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.bignerdranch.expandablerecyclerview.ExpandableRecyclerAdapter;
 import com.bignerdranch.expandablerecyclerviewsample.R;
@@ -14,17 +14,11 @@ import java.util.List;
 
 public class WeatherAdapter extends ExpandableRecyclerAdapter<CurrentDay, Hour, RecipeViewHolder, IngredientViewHolder> {
 
-    private static final int PARENT_VEGETARIAN = 0;
-    private static final int PARENT_NORMAL = 1;
-    private static final int CHILD_VEGETARIAN = 2;
-    private static final int CHILD_NORMAL = 3;
 
     private final LayoutInflater layoutInflater;
-    private final List<CurrentDay> dayList;
 
     public WeatherAdapter(Context context, @NonNull List<CurrentDay> currentDayList) {
         super(currentDayList);
-        dayList = currentDayList;
         layoutInflater = LayoutInflater.from(context);
     }
 
@@ -32,34 +26,14 @@ public class WeatherAdapter extends ExpandableRecyclerAdapter<CurrentDay, Hour, 
     @NonNull
     @Override
     public RecipeViewHolder onCreateParentViewHolder(@NonNull ViewGroup parentViewGroup, int viewType) {
-        View recipeView;
-        switch (viewType) {
-            default:
-            case PARENT_NORMAL:
-                recipeView = layoutInflater.inflate(R.layout.recipe_view, parentViewGroup, false);
-                break;
-            case PARENT_VEGETARIAN:
-                recipeView = layoutInflater.inflate(R.layout.vegetarian_recipe_view, parentViewGroup, false);
-                break;
-        }
-        return new RecipeViewHolder(recipeView);
+        return new RecipeViewHolder(layoutInflater.inflate(R.layout.item_day, parentViewGroup, false));
     }
 
     @UiThread
     @NonNull
     @Override
     public IngredientViewHolder onCreateChildViewHolder(@NonNull ViewGroup childViewGroup, int viewType) {
-        View ingredientView;
-        switch (viewType) {
-            default:
-            case CHILD_NORMAL:
-                ingredientView = layoutInflater.inflate(R.layout.ingredient_view, childViewGroup, false);
-                break;
-            case CHILD_VEGETARIAN:
-                ingredientView = layoutInflater.inflate(R.layout.vegetarian_ingredient_view, childViewGroup, false);
-                break;
-        }
-        return new IngredientViewHolder(ingredientView);
+        return new IngredientViewHolder(layoutInflater.inflate(R.layout.ingredient_view, childViewGroup, false));
     }
 
     @UiThread
@@ -73,20 +47,4 @@ public class WeatherAdapter extends ExpandableRecyclerAdapter<CurrentDay, Hour, 
     public void onBindChildViewHolder(@NonNull IngredientViewHolder ingredientViewHolder, int parentPosition, int childPosition, @NonNull Hour hour) {
         ingredientViewHolder.bind(hour);
     }
-
-    @Override
-    public int getParentViewType(int parentPosition) {
-        return PARENT_NORMAL;
-    }
-
-    @Override
-    public int getChildViewType(int parentPosition, int childPosition) {
-        return CHILD_NORMAL;
-    }
-
-    @Override
-    public boolean isParentViewType(int viewType) {
-        return viewType == PARENT_VEGETARIAN || viewType == PARENT_NORMAL;
-    }
-
 }
